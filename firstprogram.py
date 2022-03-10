3ccd50797076cc3a8b6ffdcd6acdb3d315d82204
Bharat0608 <101309730+Bharat0608@users.noreply.github.com>
2022-03-10 21:20:42 +0530

    Add files via upload

---
+++ firstprogram.py
@@ -0,0 +1,28 @@
+print('Hello world')
+x = 10 + 5j
+print(type(x))
+print(x.imag)
+print(x.conjugate())
+
+#Strings
+
+channelName="Bharat Sunil"
+print(channelName)
+print(len(channelName))
+
+#Accessing elements of string indexing
+
+print(channelName[0])
+print(channelName[-1])
+
+#Accessing elements of string slicing
+
+print(channelName[1:5])
+print(channelName[1:5:2])
+print("Bharat" * "awesome")
+
+
+
+
+
+
